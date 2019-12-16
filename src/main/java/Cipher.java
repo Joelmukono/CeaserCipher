18faa0977238e79fb4c6265fb3cf51c1ce2e5e53
Joelmukono <jlmukono@gamil.com>
2019-12-16 11:55:25 +0300

    Create project

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,34 @@
+public class Cipher {
+
+    static String  alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    static char[] letter = alphabet.toCharArray();
+
+
+    static String finalWord = "";
+
+
+
+    public static void main(String args[]){
+        eachChar("joelmukono");
+
+    }
+
+    public static void  eachChar(String sentence) {
+
+
+
+             }
+
+         }
+
+
+
+
+
+
+
+    }
+
+    }
+
