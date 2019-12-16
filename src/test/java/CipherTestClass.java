8ab5f65b7bf3f89c1bd76bbd64e9069dceffc4c2
Joelmukono <jlmukono@gamil.com>
2019-12-16 12:08:47 +0300

    create test for creating an instance of a class

--- src/test/java/CipherTestClass.java
+++ src/test/java/CipherTestClass.java
@@ -1,6 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CipherTestClass {
+
+    @Test
+    public void instance_getInstance_returnTrue(){
+        Cipher newCipher = new Cipher("L",3);
+        assertEquals(true, newCipher instanceof Cipher);
+    }
 
 
 }
