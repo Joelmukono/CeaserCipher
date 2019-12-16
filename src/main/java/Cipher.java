afe59a9449a6dde2dddf9af72c80f0cd31c03ac4
Joelmukono <jlmukono@gamil.com>
2019-12-16 12:01:49 +0300

    create constructor with private varriables

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,4 +1,14 @@
 class Cipher{
 
+    private String textToEncrypt;
+    private int keyValue;
+
+
+    public Cipher(String text,int key){
+        this.textToEncrypt = text;
+        this.keyValue = key;
+
+    }
+
 }
 
