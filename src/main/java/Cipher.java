b8d6fa5933707310c4d6ccd05531ce9ae3360ca8
Joelmukono <jlmukono@gamil.com>
2019-12-16 11:58:53 +0300

    create cipher class

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,34 +1,4 @@
-public class Cipher {
+class Cipher{
 
-    static String  alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    static char[] letter = alphabet.toCharArray();
-
-
-    static String finalWord = "";
-
-
-
-    public static void main(String args[]){
-        eachChar("joelmukono");
-
-    }
-
-    public static void  eachChar(String sentence) {
-
-
-
-             }
-
-         }
-
-
-
-
-
-
-
-    }
-
-    }
+}
 
